e52bdfc19cc56470b8bfc2676aa2f8ec85d43a94
Jack Cascarini <11981133+cascabeanie@users.noreply.github.com>
2025-02-06 12:46:36 +0000

    refactor: update dynamic rendering of work experience section

--- components/main/work.tsx
+++ components/main/work.tsx
@@ -1,4 +1,4 @@
-import Stylist from "@/public/svg/stylist";
+import { work } from "@/data/constants/work";
 
 export default function Work() {
   return (
@@ -13,49 +13,27 @@ export default function Work() {
 
         <div className="max-w-4xl rounded-lg border border-white">
           <ul className="ml-10 border-l border-white">
-            <li className="relative ml-10 py-4 pr-2 md:pr-4">
-              <span className="absolute top-4 -left-[4.25rem] rounded-full bg-white p-1">
-                <Stylist size={48} />
-              </span>
-              <div className="flex flex-col gap-1">
-                <time className="text-xs font-light text-gray-400 md:text-sm">
-                  Feb 2025 - Feb 2025
-                </time>
-                <h3 className="text-xl font-semibold text-gray-100 md:text-2xl">
-                  Stylist (DC Thomson)
-                </h3>
-                <p className="text-base font-light text-gray-300 md:text-lg">
-                  Front End Work Experience
-                </p>
-                <p className="text-base font-normal text-gray-100 md:text-lg">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Perspiciatis voluptatem cum ut maiores debitis autem inventore
-                  laudantium, assumenda modi consequuntur molestiae dolorem
-                  sapiente quam eaque ad, accusamus ipsam alias? Suscipit.
-                </p>
-              </div>
-            </li>
-
-            <li className="relative ml-10 py-4 pr-2 md:pr-4">
-              <span className="absolute top-4 -left-[4.25rem] rounded-full bg-white p-1">
-                <Stylist size={48} />
-              </span>
-              <div className="flex flex-col gap-1">
-                <time className="text-xs font-light text-gray-400 md:text-sm">
-                  04 Nov 2024 - 8 Nov 2024
-                </time>
-                <h3 className="text-xl font-semibold text-gray-100 md:text-2xl">
-                  Stylist (DC Thomson)
-                </h3>
-                <p className="text-base font-light text-gray-300 md:text-lg">
-                  Front End Work Experience
-                </p>
-                <p className="text-base font-normal text-gray-100 md:text-lg">
-                  Gained practical experience with agile software development on
-                  industry projects.
-                </p>
-              </div>
-            </li>
+            {work.map((job) => (
+              <li key={job.id} className="relative ml-10 py-4 pr-2 md:pr-4">
+                <span className="absolute top-4 -left-[4.25rem] rounded-full bg-white p-1">
+                  <job.icon size={48} />
+                </span>
+                <div className="flex flex-col gap-1">
+                  <time className="text-xs font-light text-gray-400 md:text-sm">
+                    {job.date}
+                  </time>
+                  <h3 className="text-xl font-semibold text-gray-100 md:text-2xl">
+                    {job.name}
+                  </h3>
+                  <p className="text-base font-light text-gray-300 md:text-lg">
+                    {job.title}
+                  </p>
+                  <p className="text-base font-normal text-gray-100 md:text-lg">
+                    {job.description}
+                  </p>
+                </div>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
---
+++ data/constants/work.ts
@@ -0,0 +1,22 @@
+import Stylist from "@/public/svg/stylist";
+
+export const work = [
+  {
+    id: 1,
+    name: "Stylist (DC Thomson)",
+    title: "Front End Work Experience",
+    date: "Feb 2025 - Feb 2025",
+    icon: Stylist,
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis voluptatem cum ut maiores debitis autem inventore laudantium, assumenda modi consequuntur molestiae dolorem sapiente quam eaque ad, accusamus ipsam alias? Suscipit.",
+  },
+  {
+    id: 2,
+    name: "Stylist (DC Thomson)",
+    title: "Front End Work Experience",
+    date: "04 Nov 2024 - 8 Nov 2024",
+    icon: Stylist,
+    description:
+      "Gained practical experience with agile software development on industry projects.",
+  },
+];
